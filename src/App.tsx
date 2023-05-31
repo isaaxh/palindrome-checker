5f0c4fcb59ecec2ffbd1784350d35825a1ef0f59
Isaac Hussain <isaaclearns58@gmail.com>
2023-05-31 22:21:14 +0300

    reverse the string

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+// get the word - done
+// Split the word into an array
+// have a reversed array of the same word
+// Compare each characters
+
 const App = () => {
+  const [inputText, setInputText] = useState<string>();
+  const [textArr, setTextArr] = useState<string[] | undefined>([]);
+  const [reversedArr, setReversedArr] = useState<string[] | undefined>([]);
+
+  const splitTextToArray = (text: string | undefined) => {
+    const textArray = text?.split("");
+
+    setTextArr(textArr);
+    reverseTextArray(textArray);
+  };
+
+  const reverseTextArray = (textArr: string[] | undefined) => {
+    const reversedArr: string[] = [];
+
+    textArr?.forEach((char) => {
+      reversedArr.unshift(char);
+    });
+
+    setReversedArr(reversedArr);
+  };
+
+  const handleBtnClick = (text: string | undefined) => {
+    splitTextToArray(text);
+  };
+
+  useEffect(() => {
+    console.log(reversedArr);
+  }, [reversedArr]);
+
   return (
     <div className='container'>
       <div className='question-container'>
@@ -9,6 +44,7 @@ const App = () => {
           <input
             className='input-text'
             type='text'
+            onChange={(e) => setInputText(e.target.value)}
             placeholder='Enter a text here...'
           />
         </div>
@@ -18,7 +54,11 @@ const App = () => {
         <div className='output'></div>
       </div>
       <div className='btn-container'>
-        <button className='btn-reverse' type='button'>
+        <button
+          className='btn-reverse'
+          type='button'
+          onClick={() => handleBtnClick(inputText)}
+        >
           Check
         </button>
       </div>
